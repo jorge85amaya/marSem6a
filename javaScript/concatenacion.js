bf27316f59d92447211fdb2f9faceacfbe46ec46
Jorge Amaya <jorgebenildoamaya@gmail.com>
2022-11-26 20:13:48 -0300

    Concatenacion

---
+++ javaScript/concatenacion.js
@@ -0,0 +1,7 @@
+let nombre = "Programacion" + " Fácil";
+let saludo = "Les doy la bienvenida a ";
+let fraseCompleta = saludo + nombre;
+console.log(fraseCompleta);
+let agradecimiento = ". Gracias por elegirnos";
+fraseCompleta = fraseCompleta + agradecimiento;
+console.log(fraseCompleta);
